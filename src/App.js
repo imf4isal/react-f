ea2d2f4eae84bccafd2ad8339687554af7ce6e39
Faisal <md.faisal1899@yahoo.com>
2022-01-15 10:24:30 +0600

    added useEffect(checking isLoggedIn, and added validation on every key stroke)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React, { useState } from 'react';
+import React, {useEffect, useState} from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const storedUserLogedInInformation = localStorage.getItem("isLogedIn");
+
+  useEffect(() => {
+    if (storedUserLogedInInformation) setIsLoggedIn(true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+
+    localStorage.setItem("isLogedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLogedIn", "1");
+
     setIsLoggedIn(false);
   };
 
