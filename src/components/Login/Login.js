4c011474eed2485112f7e7403780f558104227d9
Faisal <md.faisal1899@yahoo.com>
2022-01-16 01:21:49 +0600

    used forward Ref for using function of component inside another another component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useReducer, useContext} from "react";
+import React, {useState, useEffect, useReducer, useContext, useRef} from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
@@ -55,6 +55,9 @@ const Login = () => {
   const {isValid: emailIsValid} = emailState;
   const {isValid: passwordIsValid} = passwordState;
 
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+
   useEffect(() => {
     const identifier = setTimeout(() => {
       console.log("Checking form validity!");
@@ -91,13 +94,20 @@ const Login = () => {
 
   const submitHandler = event => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+    if (formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value);
+    } else if (!emailIsValid) {
+      emailInputRef.current.focus();
+    } else {
+      passwordInputRef.current.focus();
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+          ref={emailInputRef}
           label="Email"
           type="email"
           id="email"
@@ -107,6 +117,7 @@ const Login = () => {
           onBlur={validateEmailHandler}
         />
         <Input
+          ref={passwordInputRef}
           label="Password"
           type="password"
           id="password"
@@ -116,7 +127,7 @@ const Login = () => {
           onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
